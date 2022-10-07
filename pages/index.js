2d395bd70ac565adcd372bcbbbe256aa2429c587
Kafui Alordo <kalordo7@gmail.com>
2022-10-07 13:43:53 +0000

    add a section-card wrapper/card components, and implement framer motion with the card component

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,22 @@ import styles from "../styles/Home.module.css";
 
 import NavBar from "../components/nav/navbar";
 import Banner from "../components/banner/banner";
+import Card from "../components/card/card";
+import SectionCards from "../components/card/section-cards";
 
 export default function Home() {
+  const disneyVideos = [
+    {
+      imgUrl: "/static/clifford.jpg",
+    },
+    {
+      imgUrl: "/static/clifford.jpg",
+    },
+    {
+      imgUrl: "/static/clifford.jpg",
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,7 +34,16 @@ export default function Home() {
         subTItle="On the Come up!"
         imgUrl="/static/clifford.jpg"
       />
-      {/* <Card /> */}
+
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+
+        <SectionCards
+          title="Productivity"
+          videos={disneyVideos}
+          size="medium"
+        />
+      </div>
     </div>
   );
 }
