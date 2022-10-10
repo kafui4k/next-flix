eae5183aa9c220b6e3c7b6060bdaf9ead629ffe9
Kafui Alordo <kalordo7@gmail.com>
2022-10-10 15:56:01 +0000

    setup graphql with hasura db and create user roles with custom jwt secret

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Banner from "../components/banner/banner";
 import SectionCards from "../components/card/section-cards";
 
 import { getPopularVideos, getVideos } from "../lib/videos";
+import { startFetchMyQuery } from "../lib/db/hasura";
 
 export async function getServerSideProps() {
   const disneyVideos = await getVideos("disney trailer");
@@ -25,6 +26,7 @@ export default function Home({
   spaceExploration,
   popularVideos,
 }) {
+  startFetchMyQuery();
   return (
     <div className={styles.container}>
       <Head>
